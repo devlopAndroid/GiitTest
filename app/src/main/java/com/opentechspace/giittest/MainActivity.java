f269b544302ef0ca7a9680385b534442bcfcb200
Praveen Singh <singpraveen2010@gmail.com>
2023-09-02 22:38:46 +0530

    toast

--- app/src/main/java/com/opentechspace/giittest/MainActivity.java
+++ app/src/main/java/com/opentechspace/giittest/MainActivity.java
@@ -3,6 +3,7 @@ package com.opentechspace.giittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 }
