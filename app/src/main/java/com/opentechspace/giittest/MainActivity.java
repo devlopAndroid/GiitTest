f3645187a8f498b0569e20440dbb75c241c25dd4
Praveen Singh <singpraveen2010@gmail.com>
2023-09-02 23:11:49 +0530

    checking toast

--- app/src/main/java/com/opentechspace/giittest/MainActivity.java
+++ app/src/main/java/com/opentechspace/giittest/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "No", Toast.LENGTH_SHORT).show();
     }
 }
